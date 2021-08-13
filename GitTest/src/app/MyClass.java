38a4ff3aad24b4dc07cdb3fae280afcc5f55d480
Amos Chung <zxcy9980@gmail.com>
2021-08-13 09:26:17 +0800

    Add Math Class On Amos Branch

--- GitTest/src/app/MyClass.java
+++ GitTest/src/app/MyClass.java
@@ -3,5 +3,13 @@ package app;
 public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("1st version");
+//		這是一個新加的數學運算版本
+		Math cMath = new Math();
+		System.out.println(cMath.add(10, 5));
+	}
+}
+class Math{
+	int add (int a , int b ) {
+		return a+b;
 	}
 }
